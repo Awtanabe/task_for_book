a739728cdd386ce9457a916d220c3bb838f7d670
Awtanabe <panser098@gmail.com>
2019-10-25 20:25:05 +0900

    todo crud

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    flash[:notice] = "タスク#{task.name}を登録しました"
+    redirect_to tasks_url
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    flash[:notice] = "タスクを更新しました"
+    redirect_to tasks_url
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    flash[:notice] = "削除完了しました"
+    redirect_to tasks_url
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name,:description)
+  end
+end
