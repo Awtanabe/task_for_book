035cf046ac3e50fd859d8018c0e3fdb29100ac0f
Awtanabe <panser098@gmail.com>
2019-10-26 21:39:23 +0900

    current_user merge to params

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ApplicationRecord
- validates :name, presence: true
- 
+ validates :name, presence: true 
  validate :validate_name_not_inclueding_column
-
+ 
+ belongs_to :user
   def validate_name_not_inclueding_column
     errors.add(:name, "にカンマを含める事はできません") if name&.include?(',')
   end
