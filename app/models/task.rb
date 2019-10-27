dc0c8a498d2308786ab730f69bf347cbc87dcebf
Awtanabe <panser098@gmail.com>
2019-10-27 13:10:26 +0900

    scope データの並び替え

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,8 @@ class Task < ApplicationRecord
  validates :name, presence: true 
  validate :validate_name_not_inclueding_column
  
+ scope :recent, -> { order(created_at: :desc) }
+
  belongs_to :user
   def validate_name_not_inclueding_column
     errors.add(:name, "にカンマを含める事はできません") if name&.include?(',')
