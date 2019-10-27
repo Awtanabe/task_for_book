96149e0f27919ad8e76700089e230b2f661919e1
Awtanabe <panser098@gmail.com>
2019-10-27 16:08:04 +0900

    メイラー

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,6 +26,7 @@ class TasksController < ApplicationController
     end
 
     if @task.save
+      TaskMailer.creation_email(@task).deliver_now
       flash[:notice] = "タスク#{@task.name}を登録しました"
       redirect_to tasks_url
     else
