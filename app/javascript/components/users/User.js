31b05881bc0469185b53cbe03cd813f9adac08e4
Awtanabe <panser098@gmail.com>
2020-02-19 00:29:59 +0900

    react-image-resizer for heroku

--- app/javascript/components/users/User.js
+++ app/javascript/components/users/User.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import user from '../../assets/user.scss'
-import Image from 'react-image-resizer';
+// import Image from 'react-image-resizer';
 
 class User extends React.Component {
   constructor(props){
