5bfa48de24180b7bba60c366f962ba9861322ebe
Awtanabe <panser098@gmail.com>
2020-02-19 00:09:03 +0900

    precomple対応

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module TaskForBook
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
+    config.assets.initialize_on_precompile = false
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins "*"
