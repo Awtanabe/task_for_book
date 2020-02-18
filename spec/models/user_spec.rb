ec7037c72c42109373f8bb226d1f97f4132c8ed5
Awtanabe <panser098@gmail.com>
2020-02-18 22:27:00 +0900

    use factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "name" do
     it "name is valid" do
-      @user = User.new(name: "user1", email:"aaa@aaa.aa", password:"password")
-      @user.valid?
-      expect(@user).to be_valid   
+      user = create(:user)
+      # @user = User.new(name: "user1", email:"aaa@aaa.aa", password:"password")
+      # @user.valid?
+      # expect(@user).to be_valid   
+      user.valid?
+      expect(user).to be_valid   
     end
     it "name is invalid" do
       @user = User.new(name: "user1", email: "", password:"password")
